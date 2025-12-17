14c4404d6e4979629fa3e111ccd27c0fb9739ccd
TinhThuuu <letinhvan908@gmail.com>
2025-12-18 00:26:59 +0700

    Code

--- FrontE/dtshop/src/pages/users/homePage/index.js
+++ FrontE/dtshop/src/pages/users/homePage/index.js
@@ -4,6 +4,8 @@ import 'react-multi-carousel/lib/styles.css';
 import './style.scss'
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import 'react-tabs/style/react-tabs.css';
+import { AiOutlineEye } from "react-icons/ai";
+import { AiOutlineShoppingCart } from "react-icons/ai";
 import s1Img from 'assets/users/images/Slider/S1.jpg';
 import s2Img from 'assets/users/images/Slider/S2.jpg';
 import s3Img from 'assets/users/images/Slider/S3.jpg';
@@ -20,6 +22,10 @@ import feat4 from 'assets/users/images/Features/iphone-15-plus_1__1.jpg';
 import feat5 from 'assets/users/images/Features/samsung-galaxy-s24.jpg';
 import feat6 from 'assets/users/images/Features/samsung-galaxy-z-fold-7-xanh256.jpg';
 import feat7 from 'assets/users/images/Features/SSG_S25_U12G_256G.jpg';
+import bann1 from 'assets/users/images/bannerB/banner1.jpg';
+import bann2 from 'assets/users/images/bannerB/banner2.jpg';
+import { Link } from 'react-router-dom';
+import { formater } from 'utils/formater';
 
 
 
@@ -42,139 +48,176 @@ const responsive = {
   },
 };
 
-const sliderItem= [
-    {
-        bgImg: s1Img,
-        name:" ",
-    },
-    {
-        bgImg: s2Img,
-        name:" ",
-    },
-    {
-        bgImg: s3Img,
-        name:" ",
-    },
-    {
-        bgImg: s4Img,
-        name:" ",
-    },
-    {
-        bgImg: s5Img,
-        name:" ",
-    },
-    {
-        bgImg: s6Img,
-        name:" ",
-    },
-    {
-        bgImg: s7Img,
-        name:" ",
-    },
-    {
-        bgImg: s8Img,
-        name:" ",
-    },
-    {
-        bgImg: s9Img,
-        name:" ",
-    },
+const sliderItem = [
+  {
+    bgImg: s1Img,
+    name: " ",
+  },
+  {
+    bgImg: s2Img,
+    name: " ",
+  },
+  {
+    bgImg: s3Img,
+    name: " ",
+  },
+  {
+    bgImg: s4Img,
+    name: " ",
+  },
+  {
+    bgImg: s5Img,
+    name: " ",
+  },
+  {
+    bgImg: s6Img,
+    name: " ",
+  },
+  {
+    bgImg: s7Img,
+    name: " ",
+  },
+  {
+    bgImg: s8Img,
+    name: " ",
+  },
+  {
+    bgImg: s9Img,
+    name: " ",
+  },
 ];
 
-const featProducts ={
-    all:{
-        title: 'Toàn bộ',
-        products: [
-            {
-                img: feat1,
-                name: 'Sản Phẩm 0',
-                price:20000,
-            },
-            {
-                img: feat2,
-                name: 'Sản Phẩm 1',
-                price:20000,
-            },
-            {
-                img: feat3,
-                name: 'Sản Phẩm 2',
-                price:20000,
-            },
-        ]
-    },
-    SanPham1:{
-        title: 'Loai1',
-        products: [
-            {
-                img: feat4,
-                name: 'Sản Phẩm 3',
-                price:20000,
-            }
-        ]
-    },
+const featProducts = {
+  all: {
+    title: 'Toàn bộ',
+    products: [
+      {
+        img: feat1,
+        name: 'Sản Phẩm 0',
+        price: 20000,
+      },
+      {
+        img: feat2,
+        name: 'Sản Phẩm 1',
+        price: 20000,
+      },
+      {
+        img: feat3,
+        name: 'Sản Phẩm 2',
+        price: 20000,
+      },
+    ]
+  },
+  SanPham1: {
+    title: 'Loai1',
+    products: [
+      {
+        img: feat4,
+        name: 'Sản Phẩm 3',
+        price: 20000,
+      }
+    ]
+  },
 }
 
 const renderFeaturedProducts = (data) => {
-    const tabList=[];
-    const TabPanels=[];
-    
-    Object.keys(data).forEach((key,index)=>{
-        tabList.push(<Tab key={index}>{data[key].title}</Tab>)
-        const TabPanel=[];
-        data[key].products.forEach((item,j)=>{
-            TabPanel.push(<div key={j}>{item.name}</div>);
-        });
-        TabPanels.push(TabPanel);
-    });
-    
+  const tabList = [];
+  const TabPanels = [];
 
-    return (
-        <Tabs>
-            <TabList>{tabList}</TabList>
+  Object.keys(data).forEach((key, index) => {
+    tabList.push(<Tab key={index}>{data[key].title}</Tab>)
+    const TabPanel = [];
+    data[key].products.forEach((item, j) => {
+      TabPanel.push(<div className='col-lg-3' key={j}>
+        <div className='featured_item'>
+          <div className='featured_item_pic'
+          style={
             {
-                TabPanels.map((item,key)=>(
-                    <TabPanel key={key}>
-                        <div className='row'>
-                            {item}
-                        </div>
-                    </TabPanel>
-                ))}
-        </Tabs>
-    );
+              backgroundImage:`url(${item.img})`
+            }}>
+            <ul className='featured_item_pic_hover'>
+              <li>
+                <AiOutlineEye />
+              </li>
+              <li>
+                <AiOutlineShoppingCart />
+              </li>
+            </ul>
+          </div>
+          <div className='featured_item_text'>
+            <h6>
+              <Link to=''>{item.name}</Link>
+            </h6>
+            <h5>{formater(item.price)}</h5>
+          </div>
+        </div>
+      </div>);
+    });
+    TabPanels.push(TabPanel);
+  });
+
+
+  return (
+    <Tabs>
+      <TabList>{tabList}</TabList>
+      {
+        TabPanels.map((item, key) => (
+          <TabPanel key={key}>
+            <div className='row'>
+              {item}
+            </div>
+          </TabPanel>
+        ))}
+    </Tabs>
+  );
 };
 
 
 const HomePage = () => {
   return (
     <>
-    {/*Categories Begin*/}
-    <div className='container container__categories_slider'>
-      <Carousel responsive={responsive} className='categories_slider'>
-        {
-            sliderItem.map((item,key)=>(
-                <div 
-                className='categories_slider_item' 
-                style={{backgroundImage:`url(${item.bgImg})`}}
+      {/*Categories Begin*/}
+      <div className='container container__categories_slider'>
+        <Carousel responsive={responsive} className='categories_slider'>
+          {
+            sliderItem.map((item, key) => (
+              <div
+                className='categories_slider_item'
+                style={{ backgroundImage: `url(${item.bgImg})` }}
                 key={key}
-                >
+              >
                 <p>{item.name}</p>
-                </div>
+              </div>
             ))
-        }
-        
-      </Carousel>
-    </div>
-    {/*Categories End*/}
-    {/*Feature Begin*/}
-    <div className='container'>
+          }
+
+        </Carousel>
+      </div>
+      {/*Categories End*/}
+      {/*Feature Begin*/}
+      <div className='container'>
         <div className='featured'>
-            <div className='section-title'>
-                <h2>Sản phẩm nổi bật</h2>
-            </div>
-            {renderFeaturedProducts(featProducts)}
+          <div className='section-title'>
+            <h2>Sản phẩm nổi bật</h2>
+          </div>
+          {renderFeaturedProducts(featProducts)}
         </div>
+      </div>
+      {/*Feature End*/}
+      {/*Banner Begin*/}
+      <div className="container">
+        <div className="banner">
+          <div className="banner_pic">
+            <img src={bann1} alt="banner" />
+        </div>
+          <div className="banner_pic">
+            <img src={bann2} alt="banner" />
+        </div>
+      </div>
     </div>
-    {/*Feature End*/}
+
+      {/*Banner End*/}
+
     </>
   );
 };
